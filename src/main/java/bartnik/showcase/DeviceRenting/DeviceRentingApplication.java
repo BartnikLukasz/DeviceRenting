48948c265834086f817e70ca8e602b26d6fe3739
BartnikLukasz <bartnik.luk@gmail.com>
2021-10-07 15:45:55 +0200

    init

---
+++ src/main/java/bartnik/showcase/DeviceRenting/DeviceRentingApplication.java
@@ -0,0 +1,13 @@
+package bartnik.showcase.DeviceRenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceRentingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceRentingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bartnik/showcase/DeviceRenting/DeviceRentingApplicationTests.java
@@ -0,0 +1,13 @@
+package bartnik.showcase.DeviceRenting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceRentingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
