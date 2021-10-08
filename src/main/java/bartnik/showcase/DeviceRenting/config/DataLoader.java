43f49a5582224b016fca1d00abcb8e7c5843f4fe
BartnikLukasz <bartnik.luk@gmail.com>
2021-10-09 00:14:38 +0200

    Added exception handlers

---
+++ src/main/java/bartnik/showcase/DeviceRenting/config/DataLoader.java
@@ -0,0 +1,66 @@
+package bartnik.showcase.DeviceRenting.config;
+
+import bartnik.showcase.DeviceRenting.entities.CustomerEntity;
+import bartnik.showcase.DeviceRenting.entities.DeviceEntity;
+import bartnik.showcase.DeviceRenting.entities.PriceEntity;
+import bartnik.showcase.DeviceRenting.entities.RentEntity;
+import bartnik.showcase.DeviceRenting.repositories.CustomerRepository;
+import bartnik.showcase.DeviceRenting.repositories.DeviceRepository;
+import bartnik.showcase.DeviceRenting.repositories.PriceRepository;
+import bartnik.showcase.DeviceRenting.repositories.RentRepository;
+import bartnik.showcase.DeviceRenting.services.PriceService;
+import bartnik.showcase.DeviceRenting.services.RentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    private final DeviceRepository deviceRepository;
+    private final PriceRepository priceRepository;
+    private final CustomerRepository customerRepository;
+    private final RentRepository rentRepository;
+    private final PriceService priceService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        DeviceEntity deviceEntity = DeviceEntity.builder()
+                .name("Pralka")
+                .productionDate(LocalDate.of(2010, 10, 10))
+                .serialNumber("serial")
+                .build();
+        PriceEntity priceEntity = PriceEntity.builder()
+                .deviceEntity(deviceEntity)
+                .pricePerMinute(BigDecimal.valueOf(10d))
+                .build();
+        CustomerEntity customerEntity = CustomerEntity.builder()
+                .name("Name")
+                .email("email@email.com")
+                .surname("Surname")
+                .build();
+
+        deviceRepository.save(deviceEntity);
+        priceRepository.save(priceEntity);
+        customerRepository.save(customerEntity);
+
+        RentEntity rentEntity = RentEntity.builder()
+                .deviceEntity(deviceEntity)
+                .customerEntity(customerEntity)
+                .price(priceService.calculatePrice(
+                        deviceEntity.getName(),
+                        LocalDateTime.of(2021, 10, 10, 10, 10 ),
+                        LocalDateTime.of(2021, 10, 10, 11, 11 )))
+                .startDateTime(LocalDateTime.of(2021, 10, 10, 10, 10 ))
+                .endDateTime(LocalDateTime.of(2021, 10, 10, 11, 11 ))
+                .build();
+
+        rentRepository.save(rentEntity);
+    }
+}
---
+++ src/main/java/bartnik/showcase/DeviceRenting/config/ExceptionHandler.java
@@ -0,0 +1,29 @@
+package bartnik.showcase.DeviceRenting.config;
+
+import javassist.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RequiredArgsConstructor
+@ControllerAdvice
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final MessageProvider messageProvider;
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return ResponseEntity.badRequest().body(messageProvider.getError().get("invalidRequest"));
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
+    protected ResponseEntity<Object> handleNotFoundException(NotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(StringUtils.join(ex.getMessage(), " ", messageProvider.getError().get("notFound")));
+    }
+}
